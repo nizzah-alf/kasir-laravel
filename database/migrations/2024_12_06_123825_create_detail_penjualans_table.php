d7895034f2edba9f93cc0958b05987f9e2e1dc00
nizzah-alf <nizzahalfa@gmail.com>
2024-12-06 19:47:59 +0700

    setting migration kasir-laravel

--- database/migrations/2024_12_06_123825_create_detail_penjualans_table.php
+++ database/migrations/2024_12_06_123825_create_detail_penjualans_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('detail_penjualans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'petugas'])->default('petugas');
-            $table->rememberToken();
+            $table->unsignedInteger('PenjualanId');
+            $table->unsignedInteger('ProdukId');
+            $table->integer('JumlahProduk');
+            $table->decimal('Subtotal', 10, 2);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('detail_penjualans');
     }
 };
